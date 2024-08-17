1135795fef1de24d508030ec2b01ca1c49eafb91
Krunal022 <krunalvaghela723@gmail.com>
2024-08-17 10:35:49 +0530

    SmoothCursor*

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
 var crsr = document.querySelector("#cursor");
-var blur = document.querySelector("#cursor-blur");
+var main = document.querySelector("#main");
+var blurr = document.querySelector("#cursor-blur");
 
-document.addEventListener("mousemove", function (dets) {
-    crsr.style.left = dets.x + "px";
-    crsr.style.top = dets.y + "px";
-    blur.style.left = dets.x - 250 + "px";
-    blur.style.top = dets.y - 250 + "px";
+// document.addEventListener("mousemove", function (dets) {
+//     crsr.style.left = dets.x + "px";
+//     crsr.style.top = dets.y + "px";
+//     blur.style.left = dets.x - 250 + "px";
+//     blur.style.top = dets.y - 250 + "px";
+// });
+
+window.addEventListener("mousemove", function (dets) {
+  gsap.to(crsr, {
+    x: dets.x,
+    y: dets.y,
+    duration: 0.6,
+  });
+});
+window.addEventListener("mousemove", function (dets) {
+  gsap.to(blurr, {
+    x: dets.x,
+    y: dets.y,
+    duration: 0.3,
+  });
 });
 
 gsap.to("nav", {
-    backgroundColor: "black",
-    height: "10vh",
-    duration: 0.5,
-    scrollTrigger: {
-        trigger: "nav",
-        scroller: "body",
-        // markers:true,
-        start: "top -10%",
-        end: "top -11%",
-        scrub: 1
-    }
-})
+  backgroundColor: "black",
+  height: "10vh",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
 
 gsap.to("#main", {
-    backgroundColor: "black",
-    scrollTrigger: {
-        trigger: "#main",
-        scroller: "body",
-        // markers:true,
-        start: "top -25%",
-        end: "top -70%",
-        scrub: 2
-    }
-})
+  backgroundColor: "black",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers:true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
